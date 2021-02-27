6946be83b756fda3a0770b887d8d500c034d7d86
ruiinoue <jingshangrui422@gmail.com>
2021-02-27 11:40:24 +0900

    モジュールの作成

---
+++ create-npm/index.js
@@ -0,0 +1,28 @@
+(function () {
+  // var module = require("./module-1.js");
+  // module();
+})();
+
+(function () {
+  // var module = require("./module-2.js");
+  // console.log(module);
+})();
+
+(function () {
+  // var module = require("./module-3.js");
+  // console.log(module.square());
+})();
+
+(function () {
+  // var Square = require("./module-4.js");
+  // var obj = new Square();
+  // console.log(obj.area());
+})();
+
+(function () {
+  var { Square, Triangle } = require("./module-5.js");
+  var obj1 = new Square();
+  console.log(obj1.area());
+  var obj2 = new Triangle();
+  console.log(obj2.area());
+})();
