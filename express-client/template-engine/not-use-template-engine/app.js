3002f7fc0c3884f144b47aa1b8fe825461e44f3c
ruiinoue <jingshangrui422@gmail.com>
2021-02-28 15:45:29 +0900

    テンプレートエンジンの基本

---
+++ express-client/template-engine/not-use-template-engine/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+// テンプレートエンジンを使用しない場合
+app.get("/", (req, res) => {
+    var data = "<!DOCTYPE html>\r\n";
+    data += "<html>\r\n";
+    data += "  <head>\r\n";
+    data += "    <meta charset='UTF-8'>\r\n";
+    data += "    <title>startup</title>\r\n";
+    data += "  </head>\r\n";
+    data += "  <body>\r\n";
+    data += "    <p>Hello World</p>\r\n";
+    data += "  </body>\r\n";
+    data += "</html>";
+    res.status(200).send(data);
+});
+
+app.listen(3000);
