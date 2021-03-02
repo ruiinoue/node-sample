076ee80e441e67e58aff012047e72a61406940ef
ruiinoue <60529151+ruiinoue@users.noreply.github.com>
2021-03-02 21:05:18 +0900

    リクエストとレスポンスの情報 (#6)
    * リクエストとレスポンスの情報
    
    * POSTデータの利用
    
    * クエリ情報の利用
    
    * ヘッダー情報の利用
    
    * クッキー情報の利用
    
    * セッション情報の利用

---
+++ request-response/get-request-response/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.all("/", (req, res) => {
+    var body = "";
+    console.log(req.method);
+    console.log(req.url);
+    console.log(JSON.stringify(req.headers));
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+    req.on("end", () => {
+        console.log(body);
+    });
+    res.send("OK");
+});
